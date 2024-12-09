4eb3b4cf29503048b22b98eaf5b26c986335d7b2
Brian Cairl <briancairl@gmail.com>
2024-12-08 23:31:37 -0500

    Format all existing headers

--- include/dont/conditions.hpp
+++ include/dont/conditions.hpp
@@ -2,23 +2,19 @@
 
 #include "dont/common.hpp"
 
-namespace dont
-{
+namespace dont {
 
-template<typename QueryT, typename PackLikeT>
-struct Identity : True {};
+template <typename QueryT, typename PackLikeT> struct Identity : True {};
 
-template<typename QueryT, typename PackLikeT>
-struct IsMember : False {};
+template <typename QueryT, typename PackLikeT> struct IsMember : False {};
 
-template<typename QueryT, template<typename...> class Pack, typename... Ts>
-struct IsMember<QueryT, Pack<Ts...>> : Constant<bool, (is_same_v<QueryT, Ts> or ...)> {};
+template <typename QueryT, template <typename...> class Pack, typename... Ts>
+struct IsMember<QueryT, Pack<Ts...>>
+    : Constant<bool, (is_same_v<QueryT, Ts> or ...)> {};
 
-template<template<typename, typename> class Condition>
-struct Invert
-{
-    template<typename QueryT, typename PackLikeT>
-    struct eval : Constant<bool, !Condition<QueryT, PackLikeT>::value> {};
+template <template <typename, typename> class Condition> struct Invert {
+  template <typename QueryT, typename PackLikeT>
+  struct eval : Constant<bool, !Condition<QueryT, PackLikeT>::value> {};
 };
 
 } // namespace dont
